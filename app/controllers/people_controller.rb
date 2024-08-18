98a7a6fc9c6455b170443ad810a7f5e278b68795
Shrayas <shrayasr@gmail.com>
2024-08-18 19:20:37 +0530

    Allow to create a person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,22 @@ class PeopleController < ApplicationController
   def show
     @person = Person.find(params[:id])
   end
+
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to @person
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private def person_params
+    params.require(:person).permit(:name, :date_of_passing, :byline, :about)
+  end
 end
