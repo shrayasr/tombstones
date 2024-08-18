12ec7ab3272648edc016788781db63959d90c96c
Shrayas <shrayasr@gmail.com>
2024-08-18 20:35:21 +0530

    Implement edit. Refactor into partials

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,6 +17,20 @@ class PeopleController < ApplicationController
     end
   end
 
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      redirect_to @person
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private def person_params
     params.require(:person).permit(:name, :date_of_passing, :byline, :about)
   end
